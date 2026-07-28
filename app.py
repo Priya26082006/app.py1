eb49869b05cf526c2e3c9c1e641855510f1a3c73
Priya26082006 <priyanagar2608@gmail.com>
2026-07-28 13:27:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -113,6 +113,20 @@ query = final_prompt+user_details
 
 import base64
 
+OPTIONS = ["DELHI","NOIDA","GURUGRAM","KANPUR","LUCKNOW","BANGLORE","PUNE"]
+LOCATION = st.sidebar.multiselect('SELECT LOCATION: ' options = vOPTIONS)
+
+JOB_PROFILE = ["PYTHON DEVELOPER","GEN AI","FULL-STACK DEVELOPER","DATA ANALYST"]
+
+PROFILE = st.sidebar.multiselect("SELECT JOB ROLE",options = JOB_PROFILE)
+
+job_prompt = f"""Based on {PROFILE} jobs in {LOCATION},
+I want latest job news in using tavily,
+try top 10 search or whatever available
+and give result like naukri theme design with
+job name, job desc, salary,
+apply link and OUTPUT must be in HTML no markdowns"""
+
 if st.button('generate resume'):
   with st.spinner("runnign agent"):
 
@@ -126,6 +140,11 @@ if st.button('generate resume'):
             b64_image = base64.b64encode(img_file.read()).decode()
         data_uri = f"data:image/jpeg;base64,{b64_image}"
         code = code.replace("PROFILE_IMAGE_PLACEHOLDER", data_uri)
-      
    
     st.html(code , width="stretch" , unsafe_allow_javascript=True)
+
+    st.divider()
+    response = agent.invoke({'messages':[{'role':'user','content':job_prompt}]})
+
+    job_code = response['messages][-1].content[-1]['text']
+    st.html(job_code , width="stretch" , unsafe_allow_javascript = true
